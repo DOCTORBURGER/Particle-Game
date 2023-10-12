447c069c2539c96a19f6d4d23f6afd3910075a9f
Jonah Bogusch <jonahmbog@gmail.com>
2023-10-11 20:35:21 -0500

    Created font

--- ParticleGame/Game1.cs
+++ ParticleGame/Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,11 +10,17 @@ namespace ParticleGame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private SpriteFont _font;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            // Set the resolution
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
         }
 
         protected override void Initialize()
@@ -28,6 +35,7 @@ namespace ParticleGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _font = Content.Load<SpriteFont>("Silkscreen");
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +53,9 @@ namespace ParticleGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_font, "Test", Vector2.Zero, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ ParticleGame/Managers/SceneManager.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using ParticleGame.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleGame.Managers
+{
+    public class SceneManager : DrawableGameComponent
+    {
+        private Scene currentScene;
+
+        private readonly ContentManager content;
+
+        public SceneManager(Game game) : base(game)
+        {
+            content = new ContentManager(game.Services, "Content");
+        }
+
+        public void SetScene(Scene newScene)
+        {
+
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}
