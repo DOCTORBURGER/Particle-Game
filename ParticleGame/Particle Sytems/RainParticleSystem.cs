7737c8e815e520b1981a2e246bb71ed35aab6eac
Jonah Bogusch <jonahmbog@gmail.com>
2023-10-13 18:11:01 -0500

    Added rain sound effect

--- ParticleGame/Particle Sytems/RainParticleSystem.cs
+++ ParticleGame/Particle Sytems/RainParticleSystem.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using ParticleGame.Managers;
 
 namespace ParticleGame.Particle_Sytems
 {
-    public class RainParticleSystem : ParticleSystem
+    public class RainParticleSystem : ParticleSystem, ISoundEmitter
     {
         Rectangle _source;
 
         public override string Name => "Rain";
 
         public Vector2 Wind { get; set; } = Vector2.Zero;
+
+        private SoundEffect _rainSoundEffect;
+        private SoundEffectInstance _rainSoundInstance = null;
 
         public RainParticleSystem(Game game, Rectangle source, SceneManager sceneManager) : base(game, 2500, sceneManager)
         {
@@ -27,6 +31,27 @@ namespace ParticleGame.Particle_Sytems
             DrawOrder = AdditiveBlendDrawOrder;
         }
 
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            _rainSoundEffect = contentManager.Load<SoundEffect>("rain");
+            _rainSoundInstance = _rainSoundEffect.CreateInstance();
+            _rainSoundInstance.IsLooped = true;
+            _rainSoundInstance.Play();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _rainSoundInstance?.Stop();
+                _rainSoundInstance?.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
         protected override void InitializeParticle(ref Particle p, Vector2 where)
         {
             p.Initialize(where, new Vector2(-100, 1300), Vector2.Zero, Color.White, lifetime: 2f, rotation: 0.1f);
@@ -37,6 +62,16 @@ namespace ParticleGame.Particle_Sytems
             base.Update(gameTime);
 
             AddParticles(_source);
+        }
+
+        public void PlayNoise()
+        {
+            _rainSoundInstance?.Play();
+        }
+
+        public void StopNoise()
+        {
+            _rainSoundInstance?.Stop();
         }
     }
 }
